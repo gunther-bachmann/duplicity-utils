af4091f642b1eefdfda3ffe849673784f130de89
Gunther Bachmann <gunther.bachmann@itemis.de>
2020-03-01 21:03:02 +0100

    minor changes

--- shell.nix
+++ shell.nix
@@ -1,14 +1,11 @@
 # based on/copied from https://gitlab-extern.itemis.de/web-engineering/akka/scalish/blob/master/shell.nix
 let
   pkgs = import <nixpkgs> { };
-  unstable = import <unstable> { };
 in
 pkgs.mkShell {
   name = "duplicity-utils";
   buildInputs = with pkgs; [
-      # List packages that should be on the path
-      # You can search for package names using nix-env -qaP | grep <name>
-      racket
-      gnupg
-    ];
-  }
+    racket
+    gnupg
+  ];
+}
